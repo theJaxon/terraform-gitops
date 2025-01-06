dd0efa94071df959565ce960f58edfa73fe787ff
Jaxon <mohamedmostels@gmail.com>
2025-01-06 19:17:44 +0100

    Update TF files

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ terraform {
   backend "s3" {}
 }
 
+module "github-oidc-provider" {
+  source                   = "github.com/theJaxon/terraform-aws-github-oidc-provider"
+  github_organization_name = "theJaxon"
+  github_repository_name   = "terraform-gitops"
+}
+
+module "s3backend" {
+  source = "github.com/theJaxon/terraform-aws-s3-backend"
+  principal_arn_list = [ module.github-oidc-provider.aws_iam_role_arn ]
+}
+
 data "aws_ami" "ubuntu_ami" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
@@ -17,13 +28,52 @@ data "aws_ami" "ubuntu_ami" {
   }
 }
 
-resource "aws_instance" "ubuntu_instance" {
-  count         = 1
+resource "aws_instance" "grafana_instance" {
   ami           = data.aws_ami.ubuntu_ami.id
   instance_type = var.ec2_instance_type
 
   tags = {
-    Name        = "ubuntu",
-    Environment = "Dev"
+    Name        = "Ubuntu",
+    Environment = "Dev",
+    Service     = "Grafana"
+  }
+}
+
+resource "aws_security_group" "grafana_sg" {
+  name        = "grafana_sg"
+  description = "Allow port 3000"
+  # vpc_id      = aws_vpc.gitops_vpc.id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "Grafana-SG",
+    Environment = "Dev",
+    Service     = "Grafana"
+  }
+}
+
+check "grafana_health_check" {
+  data "http" "health_check" {
+    url = "http://${aws_instance.grafana_instance.public_ip}:${var.grafana_port}"
+    retry {
+      attempts = 5
+    }
+  }
+  assert {
+    condition     = data.http.health_check.status_code == 200
+    error_message = "Grafana is unreachable via port ${var.grafana_port}."
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "ec2_instance_type" {
   description = "Type of EC2 instance."
   default     = "t4g.micro"
 }
+
+variable "grafana_port" {
+  description = "Grafana port number."
+  default     = 3000
+}
